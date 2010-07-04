bc92055662bae3f12082f2eea21543a2fe1f177f
Trevor Power <email@trevorpower.com>
2010-07-04 18:18:10 +0100

    moved image feature to admin folder and removed a debugger command

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,7 +1,6 @@
 class ImageController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    debugger
     @image = @post.images.build(params[:file])
     @post.save()
     redirect_to @post
