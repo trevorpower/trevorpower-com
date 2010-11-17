be38d67d1fb2fb69ec31532a88eafa8d35ca32f3
trevorpower <email@trevorpower.com>
2010-11-17 01:52:10 -0800

    allow the s3 bucket name to be configured

--- config/initializers/image_store.rb
+++ config/initializers/image_store.rb
@@ -8,7 +8,7 @@ if IMAGE_STORE_CONFIG['use_s3']
     :access_key_id => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
   )
-  S3BucketName = "trevorpower-com-#{Rails.env}"
+  S3BucketName = ENV["S3_BUCKET"] || "trevorpower-com-#{Rails.env}"
   unless AWS::S3::Service.buckets.any? {|b| b.name == S3BucketName}
     AWS::S3::Bucket.create(S3BucketName, :access => :public_read) 
   end
