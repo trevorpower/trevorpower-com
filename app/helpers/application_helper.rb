77081ed5416ba6986c2fb4b8c6c835721e9db8f9
trevorpower <email@trevorpower.com>
2011-02-14 06:36:23 -0800

    use convention for navigation bat highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def area(page_area)
-    @area = page_area
+    @page = page_area
   end
-  def navigate_to(name, url, area)
-    link_to name, url, :class => @area == area ? 'ui-btn-active' : nil, :'data-ajax' => 'false'
+  def navigation_class(area)
+    (@page.to_s || controller.controller_name.downcase) == area.to_s ? 'ui-btn-active' : nil
+  end
+  def navigate_to(name, url, area = nil)
+    link_to name, url, :class => navigation_class(area || name.downcase), :'data-ajax' => 'false'
   end
   def title(page_title)
     content_for(:title) { h page_title }
