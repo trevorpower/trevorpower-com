92fcfc82ebfde2592a6e12ba149f0fd5fce05016
trevorpower <email@trevorpower.com>
2011-02-19 15:39:29 -0800

    ran 'rails new' to overwrite with rails 3 stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_trevorpower.com_session',
-  :secret      => 'e2985e5824320d698b7048bc1fe7c433c1ea18a61f85483e4845043ea85ab289dfc81748470e6d1ee21161fe00945ea3f4c0197da02e5915adacdd843db6df29'
-}
+Blog::Application.config.session_store :cookie_store, :key => '_blog_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Blog::Application.config.session_store :active_record_store
