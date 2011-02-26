3332d751a98233e5923177bd10a1ebee1b0a88ab
trevorpower <email@trevorpower.com>
2011-02-26 13:56:48 -0800

    fixed to_key with mesage model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message 
   extend ActiveModel::Naming
+  include ActiveModel::Conversion
   include ActiveModel::Validations
 
   attr_accessor :name, :address, :subject, :body
@@ -8,4 +9,8 @@ class Message
   validates_length_of :body, :maximum => 2000
   validates_presence_of :name, :address, :subject
   validates_format_of :address, :with => /([a-z0-9_.-]+)@([a-z0-9-]+)\.[a-z.]+/i
+
+  def persisted?
+    false
+  end
 end
