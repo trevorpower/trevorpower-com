c9f57c75c6e471ea1ea9069b66941ac2e157cab7
trevorpower <email@trevorpower.com>
2011-05-18 05:26:45 -0700

    tidy up comment validation markup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
+html_tag }
+
 # Initialize the rails application
 Blog::Application.initialize!
