679942eb84258bf13696471f45633c12e750a501
trevorpower <email@trevorpower.com>
2011-06-07 07:37:48 -0700

    upgrade mongomapper to 0.9

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,9 +1,3 @@
 require 'mm-multi-parameter-attributes'
 
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
-  MongoMapper.connect(RAILS_ENV)
-else
-  MongoMapper.database = "content-#{Rails.env}"
-end
 MongoMapper::Document.append_inclusions(MongoMapper::Plugins::MultiParameterAttributes)
--- features/step_definitions/blog_steps.rb
+++ features/step_definitions/blog_steps.rb
@@ -1,36 +1,38 @@
 Given /^the following posts:$/ do |posts|
-  Post.create!(posts.hashes)
+  Post.create(posts.hashes)
 end
 
 Given /^I have a post with title "([^\"]*)"$/ do |title|
-  Post.create(:title => title, :slug => Post.create_slug(title), :body => "body for this post (#{title})")
+  Post.new(:title => title, :slug => Post.create_slug(title), :body => "body for this post (#{title})").save!
 end
 
 Given /^I have a post with title "([^\"]*)" and body "([^\"]*)"$/ do |title, body|
-  Post.create(:title => title, :body => body)
+  Post.new(:title => title, :body => body).save!
 end
 
 Given /^I have a post with title "([^\"]*)" and the comments:$/ do |title, comments|
-  post = Post.create(
+  post = Post.new(
     :title => title,
     :slug => Post.create_slug(title),
     :body => "body for this post (#{title})",
     :published => true
   )
+  post.save!
   comments.hashes.each do |hash| 
     comment = post.comments.build(hash)
-    comment.save
+    comment.save!
   end
-  post.save
+  post.save!
 end
 
 Given /^I have a post with title "([^\"]*)" and (\d+) comments$/ do |title, count|
-  post = Post.create(
+  post = Post.new(
     :title => title,
     :slug => Post.create_slug(title),
     :body => "body for this post (#{title})",
     :published => true
   )
+  post.save!
   count.to_i.times do |i|
     comment = post.comments.build(
       :body => "#{title} - comment #{i+1}",
@@ -38,9 +40,8 @@ Given /^I have a post with title "([^\"]*)" and (\d+) comments$/ do |title, coun
       :email => "test#{i}@example.com",
       :published_on => Time.new(2011, 01, 01) + i.days
     )
-    comment.save
+    comment.save!
   end
-  post.save
 end
 
 When /^I delete the (\d+)(?:st|nd|rd|th) blog$/ do |pos|
