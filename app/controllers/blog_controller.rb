c61af0f543a027499af2a88889d71d8aa222a4e7
Trevor Power <trevor_power@yahoo.com>
2011-08-12 23:35:45 +0100

    removed before filter methods that were hiding params and member variables

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,8 +1,5 @@
 class BlogController < ApplicationController
 
-  before_filter :get_post, :except => [:index]
-  before_filter :get_commenting_status, :except => [:index]
-
   def index
     @posts = Post.public.all
 
@@ -14,12 +11,15 @@ class BlogController < ApplicationController
   end
 
   def post
+    @post = get_post(params[:slug])
+    @allow_comments = get_commenting_status
     @comment = @post.comments.build
-    
     @comments = @post.comments.public.all
   end
 
   def comment
+    @post = get_post(params[:slug])
+    @allow_comments = get_commenting_status
     if @allow_comments 
       if ! params[:comment].nil?
         @comment = @post.comments.build(params[:comment])
@@ -27,7 +27,7 @@ class BlogController < ApplicationController
       else
         @comment = @post.comments.build
       end
-      @comments = @post.comments.all(:published => true).sort_by(&:published_on)
+      @comments = @post.comments.public.all
 
       render 'post'
     else
@@ -35,12 +35,12 @@ class BlogController < ApplicationController
     end
   end
 
-  def get_post
-    @post = Post.find_by_slug(params[:slug])
+  def get_post(slug)
+    Post.find_by_slug(slug)
   end
 
   def get_commenting_status
-    @allow_comments = (!ENV['commenting_active'].nil?) && @post.open_for_commenting?
+    (!ENV['commenting_active'].nil?) && @post.open_for_commenting?
   end
 
 end
