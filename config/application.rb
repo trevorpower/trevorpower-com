47aaaa273e6c2cc108c3939d581c4cd7d6c10f64
Trevor Power <trevor_power@yahoo.com>
2011-12-29 15:48:44 +0000

    disable initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module Blog
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.initialize_on_precompile = false
+
     config.assets.enabled = true
 
     config.assets.version = '1.0'
