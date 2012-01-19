a386eff7c6c27971ab527822ec68463f0181fbc4
Trevor Power <trevor_power@yahoo.com>
2012-01-19 22:15:52 +0000

    fix precompilation of scss files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,8 @@ Blog::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( public.js public.scss )
-  config.assets.precompile += %w( admin.js admin.scss )
+  config.assets.precompile += %w( public.js public.css )
+  config.assets.precompile += %w( admin.js admin.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
